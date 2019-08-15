fa1e5512ec1e2b4785919002683db6615f92b7b3
Guilherme <guilhermeddc@gmail.com>
2019-08-15 18:32:50 -0300

    vrete temolates

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import UserListView
+from .views import UserListView, test
 
 urlpatterns = [
     path('', UserListView.as_view(), name='index'),
+    path('test/', test, name='test'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # {% url 'detail' slug=Client.slug %} or {{ Client.get_absolute_url }}
--- core/views.py
+++ core/views.py
@@ -6,3 +6,9 @@ from .models import User
 class UserListView(ListView):
     queryset = User.objects.all()
     template_name = 'pages/home_page.html'
+
+
+def test(request):
+    return render(request, 'signup-page.html')
+
+# profile-page.html
